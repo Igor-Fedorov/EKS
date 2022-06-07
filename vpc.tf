1f820e9e7428c919bd53b374d136e06a1782856d
Ihar Fiodarau <i.fiodarau@a1.by>
2022-06-07 18:14:40 +0300

    Add cred2

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ variable "region" {
 provider "aws" {
   region = "us-east-1"
   access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "	AWS_SECRET_ACCESS_KEY"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
 
 }
 
